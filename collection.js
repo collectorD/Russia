f7892dd1c93972bc3f913c539fd37f53f09bc0d9
collectorD <collectorD@yandex.com>
2015-04-15 15:56:36 +0200

    Update collection.js

--- collection.js
+++ collection.js
@@ -2,7 +2,7 @@
 { "moduleId" : "russiafan/yandex"},
 { "moduleId" : "mapman100/Here"},
 { "moduleId" : "wazemap/waze"},
-{ "moduleId" : "Fritz911/osm"},
+{ "moduleId" : "jamesbond002/osm"},
 { "moduleId" : "mapman100/Blitzer"},
-{ "moduleId" : "wikifan/wikimap"}
+{ "moduleId" : "wazemap/wikimap"}
 ]
