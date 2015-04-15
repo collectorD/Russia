fc7bbec364e9dbe240839c4229f7950f986ded99
collectorD <collectorD@yandex.com>
2015-04-15 15:49:01 +0200

    Create collection.js

---
+++ collection.js
@@ -0,0 +1,8 @@
+[
+{ "moduleId" : "russiafan/yandex"},
+{ "moduleId" : "mapman100/Here"},
+{ "moduleId" : "wazemap/waze"},
+{ "moduleId" : "Fritz911/osm"},
+{ "moduleId" : "mapman100/Blitzer"},
+{ "moduleId" : "wikifan/wikimap"}
+]
